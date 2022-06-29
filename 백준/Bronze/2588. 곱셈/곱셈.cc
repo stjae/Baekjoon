607bbc2d92c37b0f6ff2370a3c2951b7d299ae30
stjae <60866047+stjae@users.noreply.github.com>
2022-06-29 23:20:54 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int x, y;
+    int y_100, y_10, y_1;
+
+    std::cin >> x;
+    std::cin >> y;
+
+    y_100 = y / 100;
+    y_10 = (y - (y_100 * 100)) / 10;
+    y_1 = y - (y_100 * 100 + y_10 * 10);
+
+    std::cout << x * y_1 << "\n"
+              << x * y_10 << "\n"
+              << x * y_100 << "\n"
+              << x * y << std::endl;
+}
