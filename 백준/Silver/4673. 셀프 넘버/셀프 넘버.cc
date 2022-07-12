91e645e5804c2b84a7aaf6a0164011d02949a1e4
stjae <60866047+stjae@users.noreply.github.com>
2022-07-12 09:57:52 +0900

    [Silver V] Title: 셀프 넘버, Time: 96 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -6,25 +6,12 @@ int num[10000], gen[10000];
 void genNum()
 {
     for (int i = 0; i < 10000; i++)
+    {
         num[i] = i + 1;
+    }
     for (int j = 0; j < 10000; j++)
     {
-        if (num[j] < 10)
-        {
-            gen[j] = num[j] + num[j];
-        }
-        else if (num[j] >= 10 && num[j] < 100)
-        {
-            gen[j] = num[j] + num[j] / 10 + num[j] % 10;
-        }
-        else if (num[j] >= 100 && num[j] < 1000)
-        {
-            gen[j] = num[j] + num[j] / 100 + (num[j] % 100) / 10 + num[j] % 10;
-        }
-        else
-        {
-            gen[j] = num[j] + num[j] / 1000 + (num[j] % 1000) / 100 + (num[j] % 100) / 10 + num[j] % 10;
-        }
+        gen[j] = num[j] + num[j] / 1000 + (num[j] % 1000) / 100 + (num[j] % 100) / 10 + num[j] % 10;
     }
 }
 
